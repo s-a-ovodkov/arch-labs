80851144b87b42ab2f8f8682e1a97edc1bb38aa7
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2021-07-25 18:49:54 +0300

    Тест поднятия контекста

---
+++ task02/Application/src/test/java/ru/otus/ovodkov/task02/ApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.otus.ovodkov.task02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
