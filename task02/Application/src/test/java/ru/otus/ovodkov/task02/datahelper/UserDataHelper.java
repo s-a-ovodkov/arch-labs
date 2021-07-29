6a57c16f7497e822bb4ebdd7ec79e80ca53772b0
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2021-07-29 19:22:35 +0300

    Добавлен методы очистки БД и переработана логика метод генерации пользователей

--- task02/Application/src/test/java/ru/otus/ovodkov/task02/datahelper/UserDataHelper.java
+++ task02/Application/src/test/java/ru/otus/ovodkov/task02/datahelper/UserDataHelper.java
@@ -2,6 +2,7 @@ package ru.otus.ovodkov.task02.datahelper;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import ru.otus.ovodkov.task02.dto.UserProfile;
 import ru.otus.ovodkov.task02.models.User;
 import ru.otus.ovodkov.task02.repositories.UserRepository;
 
@@ -23,18 +24,39 @@ public class UserDataHelper {
   public static final String PHONE = "+79001112233";
 
   public User createUser() {
-    User user = new User();
+    User user = creatingUserWithoutId();
     user.setId(USER_ID);
+    return user;
+  }
+
+  public UserProfile createUserProfile() {
+    UserProfile userProfile = new UserProfile();
+    userProfile.setUsername(USERNAME);
+    userProfile.setFirstName(FIRSTNAME);
+    userProfile.setLastName(LASTNAME);
+    userProfile.setEmail(EMAIL);
+    userProfile.setPhone(PHONE);
+    return userProfile;
+  }
+
+  public Long createAndSave() {
+    User user = creatingUserWithoutId();
+    userRepository.save(user);
+    return user.getId();
+  }
+
+  public void deleteUsers() {
+    userRepository.deleteAll();
+    userRepository.flush();
+  }
+
+  private User creatingUserWithoutId() {
+    User user = new User();
     user.setUsername(USERNAME);
     user.setFirstName(FIRSTNAME);
     user.setLastName(LASTNAME);
     user.setEmail(EMAIL);
     user.setPhone(PHONE);
     return user;
-  }
-
-  public void createAndSave() {
-    User user = createUser();
-    userRepository.save(user);
   }
 }
